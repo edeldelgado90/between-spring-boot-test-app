e7d3ce726e56b1de12451689386c16f66caa8772
edelgado <edel.delgado@mercadolibre.com>
2025-01-04 02:18:26 -0300

    Add locale app config

--- src/main/java/com/between/springboot/TestappApplication.java
+++ src/main/java/com/between/springboot/TestappApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestappApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(TestappApplication.class, args);
+  }
 }
