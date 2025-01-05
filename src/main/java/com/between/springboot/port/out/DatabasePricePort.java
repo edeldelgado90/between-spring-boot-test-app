960d8465b530c2266f2550acbc4d27aa7bcdd05f
edelgado <edel.delgado@mercadolibre.com>
2025-01-05 03:47:17 -0300

    Update price service function name

--- src/main/java/com/between/springboot/port/out/DatabasePricePort.java
+++ src/main/java/com/between/springboot/port/out/DatabasePricePort.java
@@ -5,5 +5,5 @@ import java.time.LocalDateTime;
 import reactor.core.publisher.Flux;
 
 public interface DatabasePricePort extends DatabasePort<Price> {
-  Flux<Price> getPrices(Long productId, Long brandId, LocalDateTime date);
+  Flux<Price> getCurrentPriceByProductAndBrand(Long productId, Long brandId, LocalDateTime date);
 }
