cae3bb93bf623b8ccdde6eb9a10b064a5ac0c2e6
edelgado <edel.delgado@mercadolibre.com>
2025-01-07 03:10:15 -0300

    Update mapper functionalities

--- src/main/java/com/between/springboot/application/mapper/PriceMapper.java
+++ src/main/java/com/between/springboot/application/mapper/PriceMapper.java
@@ -1,13 +1,18 @@
 package com.between.springboot.application.mapper;
 
 import com.between.springboot.adapter.out.model.PriceEntity;
+import com.between.springboot.application.mapper.dto.PriceDTO;
 import com.between.springboot.domain.price.Price;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface PriceMapper {
 
-  Price toModel(PriceEntity price);
+  Price fromPriceEntityToPrice(PriceEntity price);
 
-  PriceEntity toEntity(Price price);
+  PriceEntity fromPriceToPriceEntity(Price price);
+
+  Price fromPriceDTOToPrice(PriceDTO priceDTO);
+
+  PriceDTO fromPriceToPriceDTO(Price price);
 }
